cffe4629cbd4a89cc79fe62986f61bb53ce3e586
121210119 IAN EVANGELISTA RODRIGUES <ian.rodrigues@lcc3-072.lcc.ufcg.edu.br>
2022-10-26 09:47:04 -0300

    infernooooooooooOOO

---
+++ LAB3-agenda/agenda-lab3/src/agenda/Contato.java
@@ -0,0 +1,19 @@
+package agenda;
+
+public class Contato {
+	private String nome;
+	private String sobrenome;
+	private int telefone;
+	
+	
+	public Contato(String nome, String sobrenome, int telefone) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.telefone = telefone;
+	}
+	
+	public boolean equals(Contato contato) {
+		
+	}
+
+}
